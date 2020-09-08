4d93b2c471003d5624893419873114c75f527f9e
bentoco <bentoco@outlook.com.br>
2020-09-08 13:40:14 -0300

    A sucrase transforms exemple

--- dist/server.js
+++ dist/server.js
@@ -1,12 +1,7 @@
-"use strict";require("dotenv").config();
-const express = require("express")
-const router = require("./routes");
-const app = express();
+"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _app = require('./app'); var _app2 = _interopRequireDefault(_app);
+require("dotenv").config();
 const port = process.env.SERVER_PORT;
 
-
-app.use(router);
-
-app.listen(port, () => {
+_app2.default.listen(port, () => {
   console.log(`Server listening on ${port}...`);
 });
